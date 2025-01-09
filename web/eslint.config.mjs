476828d2874b584efa53958afa60f666c77eee15
jp <sainijaproz@gmail.com>
2025-01-09 03:26:58 -0500

    :pencil2: logo and other stuff

--- web/eslint.config.mjs
+++ web/eslint.config.mjs
@@ -6,11 +6,20 @@ const __filename = fileURLToPath(import.meta.url);
 const __dirname = dirname(__filename);
 
 const compat = new FlatCompat({
-  baseDirectory: __dirname,
+    baseDirectory: __dirname,
 });
 
 const eslintConfig = [
-  ...compat.extends("next/core-web-vitals", "next/typescript"),
+    ...compat.extends("next/core-web-vitals", "next/typescript"),
+    {
+        rules: {
+            "@typescript-eslint/no-empty-object-type": "off",
+            "@typescript-eslint/no-explicit-any": "off",
+            "@next/next/no-html-link-for-pages": "off",
+            "@next/next/no-img-element": "off",
+            "react/no-unescaped-entities": "off",
+        },
+    },
 ];
 
 export default eslintConfig;
--- web/src/pages/_document.tsx
+++ web/src/pages/_document.tsx
@@ -1,13 +1,18 @@
 import { Html, Head, Main, NextScript } from "next/document";
 
 export default function Document() {
-  return (
-    <Html lang="en">
-      <Head />
-      <body className="antialiased">
-        <Main />
-        <NextScript />
-      </body>
-    </Html>
-  );
+    return (
+        <Html lang="en">
+            <Head>
+                <link
+                    href="https://fonts.cdnfonts.com/css/menlo"
+                    rel="stylesheet"
+                />
+            </Head>
+            <body className="subpixel-antialiased">
+                <Main />
+                <NextScript />
+            </body>
+        </Html>
+    );
 }
